5b9828556e649263ef0f82c4d22d2bb8c6cfb06a
shoaib-ahamed <shoaibahamedshafi@gmail.com>
2026-02-05 11:58:22 +0600

    auth port changed

--- app/main.py
+++ app/main.py
@@ -105,6 +105,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8000,
+        port=10000,
         reload=True if settings.ENVIRONMENT == "development" else False
     )
